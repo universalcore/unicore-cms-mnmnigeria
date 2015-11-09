18fe1ca71a06341750f35268f15444176d630f6d
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-11-09 12:38:06 +0200

    uncommented test and made it more appropriate

--- unicorecmsmnmnigeria/tests/test_views.py
+++ unicorecmsmnmnigeria/tests/test_views.py
@@ -24,65 +24,43 @@ class TestViews(UnicoreTestCase):
         self.config = testing.setUp(settings=settings)
         self.app = self.mk_app(self.workspace, settings=settings, main=main)
 
-    # def test_homepage_page(self):
-    #     self.workspace.setup_custom_mapping(Page, {
-    #         'properties': {
-    #             'slug': {
-    #                 'type': 'string',
-    #                 'index': 'not_analyzed',
-    #             },
-    #             'language': {
-    #                 'type': 'string',
-    #             }
-    #         }
-    #     })
-    #     self.workspace.setup_custom_mapping(Localisation, {
-    #         'properties': {
-    #             'locale': {
-    #                 'type': 'string',
-    #                 'index': 'not_analyzed',
-    #             }
-    #         }
-    #     })
+    def test_homepage_page(self):
+        self.workspace.setup_custom_mapping(Page, {
+            'properties': {
+                'slug': {
+                    'type': 'string',
+                    'index': 'not_analyzed',
+                },
+                'language': {
+                    'type': 'string',
+                }
+            }
+        })
+        self.workspace.setup_custom_mapping(Localisation, {
+            'properties': {
+                'locale': {
+                    'type': 'string',
+                    'index': 'not_analyzed',
+                }
+            }
+        })
 
-    #     self.create_categories(self.workspace, count=1)
-    #     self.create_localisation(
-    #         self.workspace, 'eng_GB', image='some-uuid',
-    #         image_host='http://some.site.com',
-    #     )
+        self.create_categories(self.workspace, count=1)
+        self.create_localisation(
+            self.workspace, 'eng_GB', image='some-uuid',
+            image_host='http://some.site.com',
+        )
 
-    #     intro_page = Page({
-    #         'title': 'Homepage Intro Title', 'language': 'eng_GB',
-    #         'description': 'this is the description text',
-    #         'slug': 'homepage-intro', 'content': 'this is the body of work',
-    #         'position': 0, 'modified_at': datetime.utcnow().isoformat()})
-    #     self.workspace.save(intro_page, 'save intro')
-    #     self.workspace.refresh_index()
+        intro_page = Page({
+            'title': 'Homepage Intro Title', 'language': 'eng_GB',
+            'description': 'this is the description text',
+            'slug': 'homepage-intro', 'content': 'this is the body of work',
+            'position': 0, 'modified_at': datetime.utcnow().isoformat()})
+        self.workspace.save(intro_page, 'save intro')
+        self.workspace.refresh_index()
 
-    #     resp = self.app.get('/', status=200)
-    #     print resp.status
-    #     self.assertTrue(
-    #         '<a href="/about/">Learn more about The Girl Effect</a>'
-    #         in resp.body)
-    #     print resp.status
-    #     self.assertTrue(
-    #         '<img alt="Welcome to the Girl Effect" '
-    #         'src="http://some.site.com/VNlJN07VKnfaB6k1imziAts4n0o='
-    #         '/320x0/some-uuid"/>' in
-    #         resp.body)
+        resp = self.app.get('/', status=200)
+        self.assertTrue(
+            '<title>Malaria No More / Nigeria</title>'
+            in resp.body)
 
-    #     resp = self.app.get('/?_LOCALE_=eng_UK', status=200)
-    #     print resp.status
-    #     self.assertTrue('<a href="/">Home</a>' in resp.body)
-
-    #     resp = self.app.get('/?_LOCALE_=swa_TZ', status=200)
-    #     print resp.status
-    #     self.assertTrue(
-    #         '<a href="/about/">Jifunze zaidi kuhusu Girl Effect</a>'
-    #         in resp.body)
-
-    #     resp = self.app.get('/?_LOCALE_=swh_TZ', status=200)
-    #     print resp.status
-    #     self.assertTrue(
-    #         '<a href="/about/">Jifunze zaidi kuhusu Girl Effect</a>'
-    #         in resp.body)
